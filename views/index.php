76ca7fa55822db73cd8b28889ffc4a5d8dc2c2e9
lehuycuong <cuongmagic0109@gmail.com>
2020-06-09 17:44:14 +0700

    first commit create views,index.php

---
+++ views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Crawler</title>
+</head>
+<h1>Crawler</h1>
+
+<body>
+    <div class="containner">
+        <form action="" method="POST">
+
+            <label>Enter the address you want to crawl data</label>
+            <input type="text" class="form-control" id="inputUrl" name="url" placeholder="Enter url"> </br>
+            <button type="submit" name="submit">Submit</button>
+
+        </form>
+    </div>
+</body>
+
+</html>

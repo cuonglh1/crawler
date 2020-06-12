1da595362ff7bdd823c3aaaaaa20c4985a6fdec4
lehuycuong <cuongmagic0109@gmail.com>
2020-06-12 17:19:20 +0700

    add tag a showdata

--- views/index.php
+++ views/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -9,37 +10,17 @@
 <body>
     <div class="containner">
         <form action="" method="POST">
-
+            
             <label>Enter the address you want to crawl data</label>
             <input type="text" class="form-control" id="inputUrl" name="url" placeholder="Enter url"> </br>
             <button type="submit" name="submit">Submit</button>
+            <a href="/crawler/views/show-data.php">showdata</a>
 
         </form>
 
     </div>
 
-    <?php
-    $uri = $_POST['url'];
-    echo $uri;
-    $reTitle = '/<h1 class.*>(.*?)<\/h1>/m';
-    $reDate = '/<span class="date">(.*?)<\/span>/m';
-
-    // Initialize CURL
-    $curl = curl_init($uri);
-    // Set return
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-    $result = curl_exec($curl);
-    // Disconnect CURL, free
-    curl_close($curl);
-
-    // preg_match_all($reTitle,$result ,$matches);
-    // var_dump($matches)
-
-    preg_match_all($reDate, $result, $matches);
-
-    var_dump($matches[1][0]);
-    ?>
+    
 </body>
 
 </html>

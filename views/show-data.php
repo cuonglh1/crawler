ff28d1d150b93b52cdf724ca3b2925cccfa57d68
lehuycuong <cuongmagic0109@gmail.com>
2020-06-16 13:49:07 +0700

    edit code

--- views/show-data.php
+++ views/show-data.php
@@ -20,6 +20,12 @@
     <h3>show data</h3>
     <h3><a href="/crawler/curl/Curl.php">Return</a></h3>
 </head>
+<style>
+    .content-article-crawler {
+        height: 300px;
+        overflow: auto;
+    }
+</style>
 
 <body>
     <table border="1px black">
@@ -29,31 +35,45 @@
                 <th>title</th>
                 <th>article</th>
                 <th>date</th>
+            </tr>
         </thead>
-        <?php
-        $sql = "SELECT* FROM crawler";
-        $result = $conn->query($sql);
-        if (mysqli_num_rows($result) > 0) {
-            $rows = $result->fetch_all(MYSQLI_ASSOC);
-            foreach ($rows as $row) {
-        ?>
-                <tbody>
+        <tbody>
+            <?php
+            $sql = "SELECT* FROM crawler";
+            $result = $conn->query($sql);
+            if (mysqli_num_rows($result) > 0) {
+                $rows = $result->fetch_all(MYSQLI_ASSOC);
+                foreach ($rows as $row) {
+            ?>
+
                     <tr>
                         <th><?= $row['id'] ?></th>
-                        <td><?= $row['title'] ?></td>
-                        <td><?= $row['article'] ?></td>
-                        <td><?= $row['datetime'] ?></td>
+                        <td>
+                            <div class="content-title-crawler">
+                                <?= $row['title'] ?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="content-article-crawler">
+                                <?= $row['article'] ?>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="content-date-crawler">
+                                <?= $row['datetime'] ?>
+                            </div>
+                        </td>
 
                     </tr>
-                </tbody>
-        <?php
+        </tbody>
+<?php
+                }
+            } else {
+                echo "no record in database";
             }
-        } else {
-            echo "no record in database";
-        }
-        ?>
+?>
 
-        </tr>
+</tr>
     </table>
 
 
